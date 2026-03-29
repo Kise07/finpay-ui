eebf0a03b5d4af82828fd0ba26ebad572f14bd64
kise <kaiazmi1412@gmail.com>
2026-03-30 00:03:04 +0530

    docs: PassingProps from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
 import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
+import FeatureCard from './sections/PassingProps';
+import { Zap, Landmark, ShieldHalf } from "lucide-react";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <div className="flex gap-6 px-10 justify-content-center">
+        <FeatureCard
+          icon={<Zap size={24} />}
+          title="Free transfers"
+          description="Create a financial experience..."
+        />
+        <FeatureCard
+          icon={<Landmark size={24} />}
+          title="Free transfers"
+          description="Create a financial experience..."
+        />
+        <FeatureCard
+          icon={<ShieldHalf size={24} />}
+          title="Free transfers"
+          description="Create a financial experience..."
+        />
+      </div>
     </>
   )
 }
